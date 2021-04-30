37b1273bb15ff257b87819296684e03ac6eff628
sCerdan <santyjoell@gmail.com>
2021-04-29 21:09:43 -0300

    Se logra mostrar ERROR cuando no carga la img

--- app/src/main/java/com/example/exercise/NotebookAdapter.java
+++ app/src/main/java/com/example/exercise/NotebookAdapter.java
@@ -1,6 +1,8 @@
 package com.example.exercise;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.exercise.models.NotebookModel;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -40,7 +43,20 @@ public class NotebookAdapter extends RecyclerView.Adapter<NotebookAdapter.ViewHo
         viewHolder.title.setText(notebookModels.get(i).getTitle());
         viewHolder.desc.setText(notebookModels.get(i).getDescription());
 
-        Picasso.get().load(notebookModels.get(i).getImage()).into(viewHolder.image);
+        Picasso.get()
+                .load(notebookModels.get(i).getImage())
+                .into(viewHolder.image, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Log.i("200", "SUCCESS");
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Log.i("400", "ERROR" + e);
+                        Picasso.get().load("https://seoheronews.com/403%20error%20or%20forbidden.png").into(viewHolder.image);
+                    }
+                });
     }
 
     @Override
@@ -49,13 +65,15 @@ public class NotebookAdapter extends RecyclerView.Adapter<NotebookAdapter.ViewHo
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final ImageView error;
         private final ImageView image;
         private final TextView title;
         private final TextView desc;
 
+        @SuppressLint("ResourceType")
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
+            error = (ImageView) itemView.findViewById(R.drawable.ic_launcher_background);
             image = (ImageView) itemView.findViewById(R.id.iv_Img);
             title = (TextView) itemView.findViewById(R.id.tv_title);
             desc = (TextView) itemView.findViewById(R.id.tv_SubTitle);
